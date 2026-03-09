8bc6a28cf445fa7c1aed317884a18b87d14b5a0c
KasimovDamir <supermaillum1e@gmail.com>
2026-03-09 18:47:59 +0300

    Add files via upload

---
+++ Practikums/2PR1/TestApp/TestApp/FormFinish.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TestApp
+{
+    partial class FormFinish
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelPercent = new System.Windows.Forms.Label();
+            this.buttonRetry = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(99, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Тест завершён!";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(102, 67);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(44, 16);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.Text = "label2";
+            // 
+            // labelPercent
+            // 
+            this.labelPercent.AutoSize = true;
+            this.labelPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPercent.Location = new System.Drawing.Point(102, 99);
+            this.labelPercent.Name = "labelPercent";
+            this.labelPercent.Size = new System.Drawing.Size(44, 16);
+            this.labelPercent.TabIndex = 2;
+            this.labelPercent.Text = "label2";
+            // 
+            // buttonRetry
+            // 
+            this.buttonRetry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRetry.Location = new System.Drawing.Point(12, 144);
+            this.buttonRetry.Name = "buttonRetry";
+            this.buttonRetry.Size = new System.Drawing.Size(119, 23);
+            this.buttonRetry.TabIndex = 3;
+            this.buttonRetry.Text = "Пройти заново";
+            this.buttonRetry.UseVisualStyleBackColor = true;
+            this.buttonRetry.Click += new System.EventHandler(this.buttonRetry_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(323, 144);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(119, 23);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // FormFinish
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 179);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonRetry);
+            this.Controls.Add(this.labelPercent);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Name = "FormFinish";
+            this.Text = "Результаты теста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelPercent;
+        private System.Windows.Forms.Button buttonRetry;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
---
+++ Practikums/2PR1/TestApp/TestApp/FormQuestion.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class FormQuestion : Form
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString;
+
+        DataTable questions = new DataTable();
+
+        int currentQuestion = 0;
+        int userId;
+        int timeLeft = 1500;
+
+        public FormQuestion(int id)
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            userId = id;
+
+            LoadQuestions();
+            ShowQuestion();
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        void LoadQuestions()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter(
+                    "SELECT * FROM Questions ORDER BY QuestionOrder", conn);
+
+                da.Fill(questions);
+            }
+        }
+
+        void ShowQuestion()
+        {
+            if (currentQuestion >= questions.Rows.Count)
+            {
+                FinishTest();
+                return;
+            }
+
+            DataRow q = questions.Rows[currentQuestion];
+
+            labelQuestion.Text = q["QuestionText"].ToString();
+
+            radioButton1.Text = q["Option1"].ToString();
+            radioButton2.Text = q["Option2"].ToString();
+            radioButton3.Text = q["Option3"].ToString();
+            radioButton4.Text = q["Option4"].ToString();
+
+            labelNumber.Text = $"Вопрос {currentQuestion + 1} из {questions.Rows.Count}";
+
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+        }
+
+        void SaveAnswer()
+        {
+            int selected = 0;
+
+            if (radioButton1.Checked) selected = 1;
+            if (radioButton2.Checked) selected = 2;
+            if (radioButton3.Checked) selected = 3;
+            if (radioButton4.Checked) selected = 4;
+
+            if (selected == 0) return;
+
+            DataRow q = questions.Rows[currentQuestion];
+
+            int correct = Convert.ToInt32(q["CorrectAnswer"]);
+            bool isCorrect = selected == correct;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                string sql = @"INSERT INTO UserAnswers
+                               (UserId, QuestionId, SelectedAnswer, IsCorrect, AnswerTime)
+                               VALUES (@uid,@qid,@ans,@cor,@time)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@uid", userId);
+                cmd.Parameters.AddWithValue("@qid", q["Id"]);
+                cmd.Parameters.AddWithValue("@ans", selected);
+                cmd.Parameters.AddWithValue("@cor", isCorrect);
+                cmd.Parameters.AddWithValue("@time", 1500 - timeLeft);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        void FinishTest()
+        {
+            timer1.Stop();
+
+            int correct = 0;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT COUNT(*) FROM UserAnswers WHERE UserId=@id AND IsCorrect=1", conn);
+
+                cmd.Parameters.AddWithValue("@id", userId);
+
+                correct = (int)cmd.ExecuteScalar();
+
+                int percent = correct * 100 / questions.Rows.Count;
+
+                SqlCommand update = new SqlCommand(
+                    @"UPDATE Users 
+                      SET Score=@s, TimeSpent=@t, IsCompleted=1 
+                      WHERE Id=@id", conn);
+
+                update.Parameters.AddWithValue("@s", percent);
+                update.Parameters.AddWithValue("@t", 1500 - timeLeft);
+                update.Parameters.AddWithValue("@id", userId);
+
+                update.ExecuteNonQuery();
+
+                FormFinish f = new FormFinish(correct, questions.Rows.Count, percent);
+                f.Show();
+                this.Close();
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            SaveAnswer();
+            currentQuestion++;
+            ShowQuestion();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if (currentQuestion == 0)
+            {
+                FormStart f = new FormStart();
+                f.Show();
+                this.Close();
+                return;
+            }
+
+            currentQuestion--;
+            ShowQuestion();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+
+            labelTimer.Text = "Осталось: " +
+                TimeSpan.FromSeconds(timeLeft).ToString(@"mm\:ss");
+
+            if (timeLeft <= 0)
+            {
+                FinishTest();
+            }
+        }
+    }
+}
