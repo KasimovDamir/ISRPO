51a9c7adc1eaf69641eab9012d62a486efbff193
KasimovDamir <supermaillum1e@gmail.com>
2026-03-12 19:41:40 +0300

    Add files via upload

---
+++ Practikums/2PR1/TestApp/TestApp/FormFinish.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace TestApp
+{
+    partial class FormFinish
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelPercent = new System.Windows.Forms.Label();
+            this.buttonRetry = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.listViewUser = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(99, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Тест завершён!";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(102, 67);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(44, 16);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.Text = "label2";
+            // 
+            // labelPercent
+            // 
+            this.labelPercent.AutoSize = true;
+            this.labelPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPercent.Location = new System.Drawing.Point(102, 99);
+            this.labelPercent.Name = "labelPercent";
+            this.labelPercent.Size = new System.Drawing.Size(44, 16);
+            this.labelPercent.TabIndex = 2;
+            this.labelPercent.Text = "label2";
+            // 
+            // buttonRetry
+            // 
+            this.buttonRetry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRetry.Location = new System.Drawing.Point(12, 342);
+            this.buttonRetry.Name = "buttonRetry";
+            this.buttonRetry.Size = new System.Drawing.Size(119, 23);
+            this.buttonRetry.TabIndex = 3;
+            this.buttonRetry.Text = "Пройти заново";
+            this.buttonRetry.UseVisualStyleBackColor = true;
+            this.buttonRetry.Click += new System.EventHandler(this.buttonRetry_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(323, 342);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(119, 23);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // listViewUser
+            // 
+            this.listViewUser.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewUser.HideSelection = false;
+            this.listViewUser.Location = new System.Drawing.Point(12, 130);
+            this.listViewUser.Name = "listViewUser";
+            this.listViewUser.Size = new System.Drawing.Size(430, 188);
+            this.listViewUser.TabIndex = 5;
+            this.listViewUser.UseCompatibleStateImageBehavior = false;
+            this.listViewUser.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 41;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Фамилия";
+            this.columnHeader2.Width = 85;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Дата теста";
+            this.columnHeader3.Width = 84;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Счёт";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Затрачено времени";
+            this.columnHeader5.Width = 135;
+            // 
+            // FormFinish
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 377);
+            this.Controls.Add(this.listViewUser);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonRetry);
+            this.Controls.Add(this.labelPercent);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Name = "FormFinish";
+            this.Text = "Результаты теста";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelPercent;
+        private System.Windows.Forms.Button buttonRetry;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ListView listViewUser;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
---
+++ Practikums/2PR1/TestApp/TestApp/FormFinish.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class FormFinish : Form
+    {
+        int userId;
+        string connStr = ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString;
+
+        public FormFinish(int id, int correct, int total, int percent)
+        {
+            InitializeComponent();
+
+            userId = id;
+
+            labelResult.Text = $"Правильных ответов: {correct} из {total}";
+            labelPercent.Text = $"Результат: {percent}%";
+
+            LoadUser();
+        }
+
+        void LoadUser()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                string sql = "SELECT FirstName, LastName, TestDate, Score, TimeSpent FROM Users WHERE Id=@id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", userId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    ListViewItem item = new ListViewItem(reader["FirstName"].ToString());
+
+                    item.SubItems.Add(reader["LastName"].ToString());
+                    item.SubItems.Add(reader["TestDate"].ToString());
+                    item.SubItems.Add(reader["Score"].ToString() + "%");
+                    item.SubItems.Add(reader["TimeSpent"].ToString() + " сек");
+
+                    listViewUser.Items.Add(item);
+                }
+            }
+        }
+
+        private void buttonRetry_Click(object sender, EventArgs e)
+        {
+            FormStart f = new FormStart();
+            f.Show();
+            this.Close();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
