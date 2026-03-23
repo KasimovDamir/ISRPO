e83a753a5245b14c1517c98888f37eff8e222dc6
KasimovDamir <supermaillum1e@gmail.com>
2026-03-23 21:22:29 +0300

    Add files via upload

---
+++ Labs/LR9/FilesApp/FilesApp/SymbolsCount.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace FilesApp
+{
+    partial class SymbolsCount
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnCountUp = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnCountUp
+            // 
+            this.btnCountUp.Location = new System.Drawing.Point(93, 12);
+            this.btnCountUp.Name = "btnCountUp";
+            this.btnCountUp.Size = new System.Drawing.Size(75, 23);
+            this.btnCountUp.TabIndex = 1;
+            this.btnCountUp.Text = "Подсчитать";
+            this.btnCountUp.UseVisualStyleBackColor = true;
+            this.btnCountUp.Click += new System.EventHandler(this.btnCountUp_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(174, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(255, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(336, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите текст или выберите файл с текстом";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(15, 87);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(396, 134);
+            this.txtText.TabIndex = 6;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(15, 65);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(233, 20);
+            this.txtPath.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Количество символов в тексте";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Enabled = false;
+            this.txtCount.Location = new System.Drawing.Point(15, 249);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(153, 20);
+            this.txtCount.TabIndex = 9;
+            // 
+            // SymbolsCount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 278);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCountUp);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "SymbolsCount";
+            this.Text = "Подсчёт символов в тексте";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnCountUp;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCount;
+    }
+}
+
---
+++ Labs/LR9/FilesApp/FilesApp/SymbolsCount.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FilesApp
+{
+    public partial class SymbolsCount : Form
+    {
+        string connectionString = @"Data Source=LuM1e\SQLLUM1E;Initial Catalog=FileHistory;Integrated Security=True";
+        bool isFileLoaded = false;
+        public SymbolsCount()
+        {
+            InitializeComponent();
+            CreateDatabase();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void CreateDatabase()
+        {
+            try
+            {
+                string dbPath = @"C:\ProgramData\FileHistory.mdf";
+
+                using (SqlConnection connection = new SqlConnection(@"Data Source=LuM1e\SQLLUM1E;Integrated Security=True"))
+                {
+                    connection.Open();
+
+                    string checkDbQuery = "SELECT COUNT(*) FROM sys.databases WHERE name = 'FileHistory'";
+
+                    using (SqlCommand checkCmd = new SqlCommand(checkDbQuery, connection))
+                    {
+                        int dbExists = (int)checkCmd.ExecuteScalar();
+
+                        if (dbExists == 0)
+                        {
+                            string createDbQuery = $@"
+                    CREATE DATABASE FileHistory
+                    ON (NAME = N'FileHistory', FILENAME = '{dbPath}')
+                    LOG ON (NAME = N'FileHistory_log', FILENAME = '{dbPath.Replace(".mdf", "_log.ldf")}')";
+
+                            using (SqlCommand createCmd = new SqlCommand(createDbQuery, connection))
+                            {
+                                createCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        txtPath.Text = openFileDialog.FileName;
+
+                        string content = File.ReadAllText(openFileDialog.FileName);
+
+                        isFileLoaded = true; 
+
+                        txtText.Text = content;
+
+                        txtCount.Clear(); 
+
+                        SaveToDatabase(openFileDialog.FileName, content, 0, "Открытие");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCountUp_Click(object sender, EventArgs e)
+        {
+            int count = txtText.Text.Length;
+            txtCount.Text = count.ToString();
+
+            isFileLoaded = false; 
+
+            SaveToDatabase(txtPath.Text, txtText.Text, count, "Подсчет");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, txtText.Text);
+
+                        txtPath.Text = saveFileDialog.FileName;
+
+                        int count = txtText.Text.Length;
+
+                        SaveToDatabase(saveFileDialog.FileName, txtText.Text, count, "Сохранение");
+
+                        MessageBox.Show("Файл сохранен");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtText.Clear();
+            txtCount.Clear();
+            txtPath.Clear();
+
+            isFileLoaded = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены, что хотите выйти?", "Выход",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            if (!isFileLoaded)
+            {
+                int count = txtText.Text.Length;
+                txtCount.Text = count.ToString();
+            }
+        }
+
+        private void SaveToDatabase(string filePath, string content, int symbolCount, string operationType)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string checkTableQuery = @"
+              IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FileOperations' AND xtype='U')
+              BEGIN
+                  CREATE TABLE FileOperations (
+                      Id INT PRIMARY KEY IDENTITY(1,1),
+                      FilePath NVARCHAR(500),
+                      Content NVARCHAR(MAX),
+                      SymbolCount INT,
+                      OperationType NVARCHAR(50),
+                      OperationDate DATETIME DEFAULT GETDATE()
+                  )
+              END";
+
+                    using (var checkCommand = new SqlCommand(checkTableQuery, connection))
+                    {
+                        checkCommand.ExecuteNonQuery();
+                    }
+
+                    string insertQuery = @"
+              INSERT INTO FileOperations (FilePath, Content, SymbolCount, OperationType) 
+              VALUES (@FilePath, @Content, @SymbolCount, @OperationType)";
+
+                    using (var command = new SqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@FilePath", filePath ?? "Без пути");
+                        command.Parameters.AddWithValue("@Content", content ?? "");
+                        command.Parameters.AddWithValue("@SymbolCount", symbolCount);
+                        command.Parameters.AddWithValue("@OperationType", operationType);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Ошибка при сохранении в БД: {ex.Message}");
+            }
+        }
+    }
+}
